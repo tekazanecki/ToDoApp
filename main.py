f54b848aeeba485e077201f1cda3a4fc1cdd036e
Tomasz <45405697+tekazanecki@users.noreply.github.com>
2023-02-15 15:31:02 +0100

    Exception on edit and complete.

--- main.py
+++ main.py
@@ -1,38 +1,63 @@
-
-user_prompt = "Enter a todo:"
-
+# loading data from file
 try:
     with open('todos.txt', 'r') as file:
         todos_list = file.readlines()
+        todos_list = [todo.strip('\n') for todo in todos_list]
 except:
     print("file with save todos is not present!")
     todos_list = []
 
+# main loop
 while True:
+    # user choice
     user_action = input("Type add, show, edit, complete or exit:").strip()
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            user_todo = input(user_prompt)
-            todos_list.append(user_todo + "\n")
-            with open('todos.txt', 'a') as file:
-                file.write(user_todo + "\n")
-        case 'show' | 'display':
-            for idx, todo in enumerate(todos_list):
-                print(f"{idx+1}-{todo}", end='')
-        case 'edit':
-            user_choice = input("Chose item you want to edit:")
+    if user_action.startswith("add") or user_action.startswith("new"):
+        if user_action == 'add' or user_action == 'new':
+            user_todo = input("Enter a todo:")
+        elif user_action[3] == ' ':
+            user_todo = user_action.split(' ', 1)[1]
+        todos_list.append(user_todo)
+        with open('todos.txt', 'a') as file:
+            file.write(user_todo + "\n")
+    elif user_action == 'show' or user_action == 'display':
+        print("---------- TODO LIST ----------")
+        for idx, todo in enumerate(todos_list):
+            print(f"{idx+1}-{todo}" )
+        print("-------------------------------")
+    elif user_action == 'edit':
+        user_choice = input("Chose item you want to edit:")
+        try:
             user_choice = int(user_choice) - 1
             edited_todo = input("On what you want to change todo:", todos_list[user_choice])
             todos_list[user_choice] = edited_todo
-        case 'complete':
+            with open('todos.txt', 'w') as file:
+                for todo in todos_list:
+                    file.write(todo + "\n")
+        except IndexError:
+            print("you select task that is not on the list")
+        except ValueError:
+            print("please use number of task that you want to edit")
+    elif user_action.startswith('complete '):
+        if user_action == "complete":
             user_choice = input("Chose item you want to complete:")
-            user_choice = int(user_choice) -1
+        elif user_action[8] == ' ':
+            user_choice = user_action.split(' ', 1)[1]
+        try:
+            user_choice = int(user_choice) - 1
             todos_list.pop(user_choice)
-        case 'exit':
-            break
-        case _ :
-            print("not any cases match!")
+            with open('todos.txt', 'w') as file:
+                for todo in todos_list:
+                    file.write(todo + "\n")
+        except IndexError:
+            print("you select task that is not on the list")
+        except ValueError:
+            print("please use number of task that you want to complete")
+    elif user_action == 'exit':
+        break
+    else:
+        print("not any cases match!")
+
 
 
