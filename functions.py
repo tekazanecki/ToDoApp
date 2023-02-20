fa104f325abbfda119e264f5fa3aebf4f37369b0
Tomasz <45405697+tekazanecki@users.noreply.github.com>
2023-02-20 12:49:44 +0100

    Basic gui of todo list.

--- functions.py
+++ functions.py
@@ -1,12 +1,41 @@
+from tkinter import END
+
 def get_todos(filename: str) -> list:
     """Loading list of task from file"""
     with open(filename, 'r') as file_local:
         todos_list_local = file_local.readlines()
         return [todo_local.strip('\n') for todo_local in todos_list_local]
 
-
 def save_todos(filename: str, todos_list_local: list) -> None:
     """Saving current list of task into file"""
     with open(filename, 'w') as file_local:
         for todo_local in todos_list_local:
             file_local.write(todo_local + "\n")
+
+def add_todos(entry, listbox, todo_list, filename="todo.txt") -> None:
+    """Add new task to listbox and file"""
+    new_val = entry.get()
+    todo_list = list(todo_list.get())
+    todo_list.append(new_val)
+    entry.delete(0, END)
+    save_todos(filename, todo_list)
+    listbox.insert(END, new_val)
+
+def edit_todos(entry, listbox, todo_list, filename="todo.txt") -> None:
+    """Edit existing item in listbox and file"""
+    new_val = entry.get()
+    index_of_item = listbox.curselection()[0]
+    todo_list = list(todo_list.get())
+    todo_list[index_of_item] = new_val
+    entry.delete(0, END)
+    save_todos(filename, todo_list)
+    listbox.delete(index_of_item)
+    listbox.insert(index_of_item, new_val)
+def delete_todos(entry, listbox, todo_list, filename="todo.txt"):
+    new_val = entry.get()
+    index_of_item = listbox.curselection()[0]
+    todo_list = list(todo_list.get())
+    todo_list.remove(new_val)
+    entry.delete(0, END)
+    save_todos(filename, todo_list)
+    listbox.delete(index_of_item)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from functions import *
+import tkinter as tk
+from tkinter import ttk
 
 source_file = 'todo.txt'
 # loading data from file
@@ -8,52 +10,54 @@ except FileNotFoundError as error:
     print(error, "file with save todos is not present!")
     todos_list = []
 
-# main loop
-while True:
-    # user choice
-    user_action = input("Type add, show, edit, complete or exit:").strip()
-    user_action = user_action.strip()
 
-    if user_action.startswith("add") or user_action.startswith("new"):
-        if user_action == 'add' or user_action == 'new':
-            user_todo = input("Enter a todo:")
-        elif user_action[3] == ' ':
-            user_todo = user_action.split(' ', 1)[1]
-        todos_list.append(user_todo)
-        save_todos(source_file, todos_list)
-    elif user_action == 'show' or user_action == 'display':
-        print("---------- TODO LIST ----------")
-        for idx, todo in enumerate(todos_list):
-            print(f"{idx+1}-{todo}" )
-        print("-------------------------------")
-    elif user_action == 'edit':
-        user_choice = input("Chose item you want to edit:")
-        try:
-            user_choice = int(user_choice) - 1
-            edited_todo = input("On what you want to change todo:", todos_list[user_choice])
-            todos_list[user_choice] = edited_todo
-            save_todos(source_file, todos_list)
-        except IndexError:
-            print("you select task that is not on the list")
-        except ValueError:
-            print("please use number of task that you want to edit")
-    elif user_action.startswith('complete '):
-        if user_action == "complete":
-            user_choice = input("Chose item you want to complete:")
-        elif user_action[8] == ' ':
-            user_choice = user_action.split(' ', 1)[1]
-        try:
-            user_choice = int(user_choice) - 1
-            todos_list.pop(user_choice)
-            save_todos(source_file, todos_list)
-        except IndexError:
-            print("you select task that is not on the list")
-        except ValueError:
-            print("please use number of task that you want to complete")
-    elif user_action == 'exit':
-        break
-    else:
-        print("not any cases match!")
+window = tk.Tk()
+window.title("ToDo App")
+window.geometry("150x220")
+window.resizable(0, 0)
+
+todo_list_var = tk.Variable(value=todo_list)
+entry_var = tk.StringVar()
+
+frm_adding = tk.Frame(window)
+lbl = ttk.Label(frm_adding, text="Insert your todo")
+ent = ttk.Entry(frm_adding, textvariable=entry_var)
+frm_list = tk.Frame(window)
+listbox_todo = tk.Listbox(
+    frm_list,
+    listvariable=todo_list_var,
+    height=6,
+    selectmode=tk.SINGLE)
+
+
+
+btn_add = ttk.Button(frm_adding, text="Add", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
+                        : add_todos(entry, listbox, todo_list))
+btn_edit = ttk.Button(frm_list, text="Edit", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
+                        : edit_todos(entry, listbox, todo_list))
+btn_complete = ttk.Button(frm_list, text="Complete", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
+                        : delete_todos(entry, listbox, todo_list))
+
+frm_adding.pack()
+lbl.pack()
+ent.pack()
+btn_add.pack()
+frm_list.pack()
+listbox_todo.pack()
+btn_edit.pack()
+btn_complete.pack()
+
+def items_selected(event):
+    # get selected indices
+    selected_indices = listbox_todo.curselection()
+    ent.delete(0, END)
+    ent.insert(0, todo_list_var.get()[selected_indices[0]])
+
+
+listbox_todo.bind('<<ListboxSelect>>', items_selected)
+
+window.mainloop()
+
 
 
 
