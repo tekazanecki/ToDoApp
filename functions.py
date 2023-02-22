6be354ba8a8ab065497ce8e46d6d86bc6dae44c6
Tomasz <45405697+tekazanecki@users.noreply.github.com>
2023-02-22 08:53:16 +0100

    Refactoring of code.

--- functions.py
+++ functions.py
@@ -1,10 +1,14 @@
 from tkinter import END
 
+FILE_NAME = 'todo.txt'
+
+
 def get_todos(filename: str) -> list:
     """Loading list of task from file"""
     with open(filename, 'r') as file_local:
         todos_list_local = file_local.readlines()
         return [todo_local.strip('\n') for todo_local in todos_list_local]
+
 
 def save_todos(filename: str, todos_list_local: list) -> None:
     """Saving current list of task into file"""
@@ -12,7 +16,15 @@ def save_todos(filename: str, todos_list_local: list) -> None:
         for todo_local in todos_list_local:
             file_local.write(todo_local + "\n")
 
-def add_todos(entry, listbox, todo_list, filename="todo.txt") -> None:
+
+def items_selected(event, entry, listbox, todos) -> None:
+    # get selected indices
+    selected_indices = listbox.curselection()
+    entry.delete(0, END)
+    entry.insert(0, todos.get()[selected_indices[0]])
+
+
+def add_todos(entry, listbox, todo_list, filename=FILE_NAME) -> None:
     """Add new task to listbox and file"""
     new_val = entry.get()
     todo_list = list(todo_list.get())
@@ -21,21 +33,27 @@ def add_todos(entry, listbox, todo_list, filename="todo.txt") -> None:
     save_todos(filename, todo_list)
     listbox.insert(END, new_val)
 
-def edit_todos(entry, listbox, todo_list, filename="todo.txt") -> None:
+
+def edit_todos(entry, listbox, todo_list, filename=FILE_NAME) -> None:
     """Edit existing item in listbox and file"""
-    new_val = entry.get()
-    index_of_item = listbox.curselection()[0]
-    todo_list = list(todo_list.get())
-    todo_list[index_of_item] = new_val
-    entry.delete(0, END)
-    save_todos(filename, todo_list)
-    listbox.delete(index_of_item)
-    listbox.insert(index_of_item, new_val)
-def delete_todos(entry, listbox, todo_list, filename="todo.txt"):
-    new_val = entry.get()
-    index_of_item = listbox.curselection()[0]
-    todo_list = list(todo_list.get())
-    todo_list.remove(new_val)
-    entry.delete(0, END)
-    save_todos(filename, todo_list)
-    listbox.delete(index_of_item)
+    if listbox.curselection():
+        new_val = entry.get()
+        index_of_item = listbox.curselection()[0]
+        todo_list = list(todo_list.get())
+        todo_list[index_of_item] = new_val
+        entry.delete(0, END)
+        save_todos(filename, todo_list)
+        listbox.delete(index_of_item)
+        listbox.insert(index_of_item, new_val)
+
+
+def delete_todos(entry, listbox, todo_list, filename=FILE_NAME):
+    """Delete existing item """
+    if listbox.curselection():
+        new_val = entry.get()
+        index_of_item = listbox.curselection()[0]
+        todo_list = list(todo_list.get())
+        todo_list.remove(new_val)
+        entry.delete(0, END)
+        save_todos(filename, todo_list)
+        listbox.delete(index_of_item)
--- main.py
+++ main.py
@@ -1,63 +1,81 @@
-from functions import *
+import functions
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Scrollbar
+from tkinter import RIGHT, LEFT, BOTH
 
-source_file = 'todo.txt'
+
 # loading data from file
+todos_list = []
 try:
-    todo_list = get_todos(source_file)
+    todo_list = functions.get_todos(functions.FILE_NAME)
 except FileNotFoundError as error:
     print(error, "file with save todos is not present!")
-    todos_list = []
-
 
 window = tk.Tk()
 window.title("ToDo App")
-window.geometry("150x220")
-window.resizable(0, 0)
+window.geometry("260x190")
+window.resizable(False, False)
 
+# variables
 todo_list_var = tk.Variable(value=todo_list)
 entry_var = tk.StringVar()
 
+# structure - upper frame
 frm_adding = tk.Frame(window)
 lbl = ttk.Label(frm_adding, text="Insert your todo")
 ent = ttk.Entry(frm_adding, textvariable=entry_var)
-frm_list = tk.Frame(window)
+separator = ttk.Separator(window, orient='horizontal')
+
+# structure - lower frame
+frm_lower = tk.Frame(window)
+frm_list = tk.Frame(frm_lower)
+frm_buttons = tk.Frame(frm_lower)
+
+# structure - Listbox control
+scrollbar = Scrollbar(frm_list)
 listbox_todo = tk.Listbox(
     frm_list,
     listvariable=todo_list_var,
     height=6,
     selectmode=tk.SINGLE)
 
+listbox_todo.bind('<<ListboxSelect>>',
+                  lambda event, entry=ent, listbox=listbox_todo, todos=todo_list_var:
+                  functions.items_selected(event, entry, listbox, todos))
+listbox_todo.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox_todo.yview)
 
+# structure - buttons
+btn_add = ttk.Button(frm_buttons, text="Add",
+                     command=lambda entry=ent, listbox=listbox_todo, todos=todo_list_var:
+                     functions.add_todos(entry, listbox, todos))
+btn_edit = ttk.Button(frm_buttons, text="Edit",
+                      command=lambda entry=ent, listbox=listbox_todo, todos=todo_list_var:
+                      functions.edit_todos(entry, listbox, todos))
+btn_complete = ttk.Button(frm_buttons, text="Complete",
+                          command=lambda entry=ent, listbox=listbox_todo, todos=todo_list_var:
+                          functions.delete_todos(entry, listbox, todos))
 
-btn_add = ttk.Button(frm_adding, text="Add", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
-                        : add_todos(entry, listbox, todo_list))
-btn_edit = ttk.Button(frm_list, text="Edit", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
-                        : edit_todos(entry, listbox, todo_list))
-btn_complete = ttk.Button(frm_list, text="Complete", command=lambda entry=ent, listbox=listbox_todo, todo_list=todo_list_var
-                        : delete_todos(entry, listbox, todo_list))
+# packing
+frame_padding = {'pady': 5,
+                 'padx': 5}
+padding = {'pady': 3,
+           'padx': 3}
 
-frm_adding.pack()
-lbl.pack()
-ent.pack()
-btn_add.pack()
-frm_list.pack()
-listbox_todo.pack()
-btn_edit.pack()
-btn_complete.pack()
+# upper frame
+frm_adding.pack(**frame_padding)
+lbl.pack(**padding)
+ent.pack(**padding)
+btn_add.pack(**padding)
+separator.pack(fill='x', **padding)
 
-def items_selected(event):
-    # get selected indices
-    selected_indices = listbox_todo.curselection()
-    ent.delete(0, END)
-    ent.insert(0, todo_list_var.get()[selected_indices[0]])
-
-
-listbox_todo.bind('<<ListboxSelect>>', items_selected)
+# lower frame
+frm_lower.pack(**frame_padding)
+frm_list.pack(side=LEFT)
+scrollbar.pack(side=RIGHT, fill=BOTH, **padding)
+listbox_todo.pack(side=LEFT, fill=BOTH, **padding)
+frm_buttons.pack(side=LEFT)
+btn_edit.pack(**padding)
+btn_complete.pack(**padding)
 
 window.mainloop()
-
-
-
-
